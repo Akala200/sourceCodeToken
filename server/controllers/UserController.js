02713365591c0df35ab056022a10bcd35a8b288f
Adeniyi Akala <adeiyiakala91@gmail.com>
2020-11-11 14:18:10 +0100

    changes made

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -213,7 +213,7 @@ class UserController {
       axios
         .get(
           // eslint-disable-next-line max-len
-          'https://api.nomics.com/v1/currencies/ticker?key=demo-26240835858194712a4f8cc0dc635c7a&ids=ETH,XRP,USDT,LINK,BCH,BNB,DOT,LTC&interval=1d,30d&convert=BTC&per-page=100&page=1'
+          'https://api.nomics.com/v1/currencies/ticker?key=demo-26240835858194712a4f8cc0dc635c7a&ids=ETH,XRP,LINK,BCH,BNB,DOT,LTC&interval=1d,30d&convert=USDT&per-page=100&page=1'
         )
         .then((response) => {
           const dataGotten = response.data;
