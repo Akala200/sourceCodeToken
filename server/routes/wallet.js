7fa7706d9eb35cfc1ef4bb9b4fad9db22aefbcb7
Adeniyi Akala <adeiyiakala91@gmail.com>
2020-12-02 17:15:23 +0100

    changes made

--- server/routes/wallet.js
+++ server/routes/wallet.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import WalletController from '../controllers/WalletController';
 import verifyNumber from '../middlewares/verifyNumber';
 
-const { decodeRegToken } = require('../utils/storeToken');
+// const { decodeRegToken } = require('../utils/storeToken');
 
 const router = Router();
 
@@ -25,20 +25,20 @@ const {
 } = WalletController;
 const { numberChecker } = verifyNumber;
 
-router.post('/credit', decodeRegToken, numberChecker, initiate);
+router.post('/credit', numberChecker, initiate);
 router.post('/send', send);
 router.post('/webhook', numberChecker, webhook);
-router.get('/history', decodeRegToken, transactionHistory);
-router.get('/history/all', decodeRegToken, transactionHistoryAll);
-router.get('/balance/coin', decodeRegToken, balance);
-router.get('/balance/naira', decodeRegToken, nairaBalance);
-router.get('/callback', decodeRegToken, callback);
-router.get('/convert', decodeRegToken, convert);
-router.get('/get/coin', decodeRegToken, coinPrice);
-router.get('/wallet/all', decodeRegToken, allWallet);
-router.get('/get/bank', decodeRegToken, getBank);
-router.post('/add/bank', decodeRegToken, addBank);
-router.get('/user/bank', decodeRegToken, getUserBank);
+router.get('/history', transactionHistory);
+router.get('/history/all', transactionHistoryAll);
+router.get('/balance/coin', balance);
+router.get('/balance/naira', nairaBalance);
+router.get('/callback', callback);
+router.get('/convert', convert);
+router.get('/get/coin', coinPrice);
+router.get('/wallet/all', allWallet);
+router.get('/get/bank', getBank);
+router.post('/add/bank', addBank);
+router.get('/user/bank', getUserBank);
 router.get('/test', createTest);
 
 
