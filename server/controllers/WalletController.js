8e6186dc4dc9c6cf973722058b275df828ddd86d
Adeniyi Akala <adeiyiakala91@gmail.com>
2020-12-04 09:24:25 +0100

    new

--- server/controllers/WalletController.js
+++ server/controllers/WalletController.js
@@ -674,7 +674,7 @@ class WalletController {
 
             const amountNew = coin + user.balance;
             console.log(amountNew);
-            const limited = coin.toFixed(6)
+            const limited = coin.toFixed(6);
             const refinedCoin = limited.toString();
             const priceReturned = event.data.amount / 100;
             const _url = `https://api.luno.com/api/1/send?amount=${refinedCoin}&currency=BTC&address=${user.address}`;
