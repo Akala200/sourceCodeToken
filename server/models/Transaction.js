ba7137b810e293a17d876f29a734de2afbd90bdf
Adeniyi <adeiyiakala91@gmail.com>
2020-12-08 13:14:44 +0000

    withdraw completed

--- server/models/Transaction.js
+++ server/models/Transaction.js
@@ -2,21 +2,20 @@ import mongoose from 'mongoose';
 // import { v1 as uuid1 } from 'uuid';
 
 const transactionSchema = mongoose.Schema({
-  amount: { type: Number, required: true },
-  coins: { type: Number, required: true },
-  type: { type: String, enum: ['credit', 'debit'], required: true },
-  mode: { type: String, required: true },
-  user: { type: String, required: true },
+  amount: { type: Number, },
+  coins: { type: Number, },
+  type: { type: String, enum: ['credit', 'debit'], },
+  mode: { type: String, },
+  user: { type: String, },
   cardType: { type: String },
   lastFour: { type: String },
-  ref: { type: String, required: true },
+  ref: { type: String, },
   walletId: { type: mongoose.Schema.ObjectId, ref: 'Wallet' },
   status: {
     type: String,
     enum: ['pending', 'successful', 'failed'],
-    required: true,
   },
-  createdAt: { type: Date, required: true, default: Date.now },
+  createdAt: { type: Date, default: Date.now },
   deletedAt: { type: Date, default: null },
 });
 const Transaction = mongoose.model('Transaction', transactionSchema);
