831e5fd55a49be2435f9a7f2606df055a1e4c4f6
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2021-11-14 18:53:40 +0100

    chnages made

--- server/config/index.js
+++ server/config/index.js
@@ -4,8 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-  MONGODB_DATABASE: process.env.DB_URL_DEV || 'mongodb+srv://coin20:123456789@@@cluster0.y5nwu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  DB_TEST: process.env.DB_URL_TEST || 'mongodb+srv://coin20:123456789@@@cluster0.y5nwu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  MONGODB_DATABASE: process.env.DB_URL_DEV || 'mongodb+srv://coin20:123456789@@@cluster0.y5nwu.mongodb.net/coin?retryWrites=true&w=majority',
+  DB_TEST: process.env.DB_URL_TEST || 'mongodb+srv://coin20:123456789@@@cluster0.y5nwu.mongodb.net/coin?retryWrites=true&w=majority'
 };
 
 export default config;
